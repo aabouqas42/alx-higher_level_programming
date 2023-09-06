bfa1f88f34d43bde7d9d1449d8ff8d00e5588bfe
AissamDEV <aissamaboukassim@gmail.com>
2023-09-06 10:36:45 -0700

    done

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-i = 0
 def prt(str, n):
     print(str.format(n), end="")
-    
+
+
+i = 0
 while i <= 99:
     if i <= 9:
-        prt("0{}, ",i)
+        prt("0{}, ", i)
     elif i == 99:
-        prt("{}\n",i)
+        prt("{}\n", i)
     else:
-        prt("{}, ",i)
+        prt("{}, ", i)
     i += 1
