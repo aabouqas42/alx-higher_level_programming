32377035300da4bf87050434e529e817d0234127
AissamDEV <aissamaboukassim@gmail.com>
2023-09-06 11:08:53 -0700

    done

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    i = 0
+    uperstr = ""
+    while i < len(str):
+        char = str[i]
+        if 'a' <= char <= 'z':
+            uperstr += chr(ord(char) - 32)
+        i += 1
+    print("{}".format(uperstr))
