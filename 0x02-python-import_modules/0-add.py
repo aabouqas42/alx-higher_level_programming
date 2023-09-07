e04ab005c73dd5306261b1578df693de5fde74a9
AissamDEV <aissamaboukassim@gmail.com>
2023-09-07 11:52:59 -0700

    done

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-from add_0 import add
-print("1 + 2 = {:d}".format(add(1, 2)))
+if __name__ == "__main__":
+    from add_0 import add
+    print("1 + 2 = {:d}".format(add(1, 2)))
