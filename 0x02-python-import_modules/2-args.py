efb69a0b105e91f7a2dbb3009a0da7348e4a714e
AissamDEV <aissamaboukassim@gmail.com>
2023-09-07 12:33:00 -0700

    done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import sys
+args = sys.argv
+if len(args) == 1:
+    print("0 arguments.")
+else:
+    i = 1
+    print("{:d} arguments:".format(len(args) - 1))
+    while i < len(args):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
+        i += 1
