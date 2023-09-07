8985019af1340c01b1b63d425fea6d15cb460728
AissamDEV <aissamaboukassim@gmail.com>
2023-09-07 12:34:30 -0700

    done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if len(args) == 1:
     print("0 arguments.")
 else:
     i = 1
-    print("{:d} arguments:".format(len(args) - 1))
+    print("{} arguments:".format(len(args) - 1))
     while i < len(args):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
         i += 1
