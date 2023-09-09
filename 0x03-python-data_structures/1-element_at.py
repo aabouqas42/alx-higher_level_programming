941fcdccc9b6ad1c2de9400a6cb1abe789ae3f8a
AissamDEV <aissamaboukassim@gmail.com>
2023-09-09 07:15:43 -0700

    done

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list):
+        return None
+    else:
+        for i in range(len(my_list)):
+            if i == idx:
+                return my_list[i]
+        
