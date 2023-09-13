3d072a7ec670c6443478fd203978666632e598c8
AissamDEV <aissamaboukassim@gmail.com>
2023-09-13 11:43:14 -0700

    done

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in my_list:
+        if i == search:
+            new_list[i] = replace
+    return new_list
