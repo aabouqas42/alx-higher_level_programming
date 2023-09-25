727e49b37af02be189b7960d82854547439ea83f
AissamDEV <aissamaboukassim@gmail.com>
2023-09-25 04:50:49 -0700

    done

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        integer_value = int(value)
+        print("{:d}".format(integer_value))
+        return True
+    except ValueError as e:
+        sys.stderr.write(f"Exception: {e}\n")
+        return False
