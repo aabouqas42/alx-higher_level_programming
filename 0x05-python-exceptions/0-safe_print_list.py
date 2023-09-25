0d1abd4c8615044f4eee804372912f5a848f2d15
AissamDEV <aissamaboukassim@gmail.com>
2023-09-25 04:47:48 -0700

    done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    
     try:
         for element in my_list:
             if count < x:
                 print(element, end=" ")
                 count += 1
         print()
-    except:
+    except IndexError:
         pass
-    
     return count
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-    
     try:
         for i in range(list_length):
             try:
                 element_1 = my_list_1[i] if i < len(my_list_1) else 0
                 element_2 = my_list_2[i] if i < len(my_list_2) else 0
-                
                 if isinstance(element_1, (int, float)) and isinstance(element_2, (int, float)):
                     if element_2 != 0:
                         division_result = element_1 / element_2
@@ -18,11 +16,8 @@ def list_division(my_list_1, my_list_2, list_length):
                 else:
                     result.append(0)
                     print("wrong type")
-                
             except IndexError:
                 result.append(0)
                 print("out of range")
-    except:
-        pass
     finally:
         return result
