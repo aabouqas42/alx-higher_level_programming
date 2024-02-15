fabe449aabd4e108a493bdd36d5f48985d9b7d8d
aabouqas <Aissam520@gmail.com>
2024-02-15 20:05:19 +0100

    done

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def	add_integer(a, b=98):
+	if not isinstance(a, int):
+		print("a must be an integer")
+		return
+	if not isinstance(b, int):
+		print("b must be an integer")
+		return
 	return (a + b)
